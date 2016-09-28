94b6060a28b2ccc238b9f637e29b1fdfa3898f93
Chance Hollis <chancehollis@gmail.com>
2016-09-28 08:42:31 -0600

    added BoardState.cs for ai reasons

---
+++ Assignment1_NimGame/Models/BoardState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_NimGame.Models
+{
+    class BoardState
+    {
+        private int topRow;
+        private int middleRow;
+        private int bottomRow;
+
+        public  bool isBadState { get; set; }  // ??? better to be a numerical value?
+
+        public BoardState(int top, int mid, int bottom)
+        {
+            topRow = top;
+            middleRow = mid;
+            bottomRow = bottom;
+        }
+    }
+}
--- Assignment1_NimGame/Models/ComputerPlayer.cs
+++ Assignment1_NimGame/Models/ComputerPlayer.cs
@@ -16,7 +16,6 @@ namespace Assignment1_NimGame.Models
             return choice;
         }
 
-        
 
     }
 }
