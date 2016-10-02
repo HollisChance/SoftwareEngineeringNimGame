e46a4b3680eba24ba2d377a6500aed7860d4445a
MBalderasNu <mexico2067@gmail.com>
2016-10-02 17:33:09 -0600

    test

---
+++ Assignment1_NimGame/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_NimGame.Models
+{
+ abstract public class Player
+    {
+
+
+   abstract public Move MakeMove(Row[] rows);
+        
+    }
+}
---
+++ Assignment1_NimGame/Models/RandomAI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_NimGame.Models
+{
+    public class RandomAI : Player
+    {
+        public override Move MakeMove(Row[] rows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
